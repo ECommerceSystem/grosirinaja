50c1881696b549a7dcba3fe2b86bacb2fa2e5d06
Yosua Yuuta Bima Pramana <y_bima_p@yahoo.com>
2018-10-29 09:44:08 +0700

    add model for user and goods

---
+++ database/migrations/2018_10_29_021902_create_model_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('password');
+            $table->string('address');
+            $table->string('birth_date');
+            $table->enum('sex', ['Male', 'Female']);
+            $table->string('email');
+            $table->enum('level', ['Buyer', 'Seller', 'Admin']);
+            $table->string('telephone_number');
+            $table->rememberToken();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
